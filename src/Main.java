6953439fab9fd51123d03624185c360d74527938
zax <2114531580@qq.com>
2023-07-19 15:25:56 +0800

    加了些注释

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /**
+         * 文档注释
+         */
+        //单行注释
+        /*
+        多行注释
+        */
     }
 }
